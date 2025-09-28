de6889a9e43b81ef58469139b02c1ecbbf6ec23b
hufeng <985311845@qq.com>
2025-09-28 16:56:53 +0800

    eeee

--- src/javascript/1.语言基础/objectTest.js
+++
@@ -1,8 +0,0 @@
-function Article() {
-    this.title = 'fhjdkhashfjhdlsk';
-    this.author = 'Jake'
-};
-let a1 = new Article();
-let a2 = new Article();
-a1.author = null;
-console.log(a2)
--- src/javascript/1.语言基础/symbolType.js
+++
@@ -1,3 +0,0 @@
-const sym1 = Symbol('aaa');
-const sym2 = Symbol('aaa');
-console.log(sym1 === sym2);
