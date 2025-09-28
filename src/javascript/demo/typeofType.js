800a876d2314afc01c1c52722c767331e722133d
hufeng <985311845@qq.com>
2025-09-28 16:56:36 +0800

    ddd

--- src/javascript/demo/typeofType.js
+++
@@ -1,9 +0,0 @@
-// const map = new Map();
-// console.log(typeof map);
-// console.log(map instanceof Object)
-// console.log(Object.prototype.toString.call(map));
-// console.log(map.constructor)
-// console.log([1] == 1)
-// console.log([1].toString())
-// console.log('1' - 1)
-console.log([].valueOf().toString())
--- src/javascript/demo2/module-demo/test2.js
+++
@@ -1,3 +0,0 @@
-import { a } from './test1';
-
-console.log(a);
